e98695d74a3b5ffaaaaa18d92e662f78fb333e13
slinky57 <darredondo@unm.edu>
2021-10-06 13:59:54 -0600

    read_file added

---
+++ include/imp/read_file.hpp
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <string>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// adapted from https://www.delftstack.com/howto/cpp/read-file-into-string-cpp/
+namespace imp {
+std::string read_file(const std::string& path) {
+    struct stat sb {};
+    std::string res;
+
+    FILE* input_file = fopen(path.c_str(), "r");
+    if (input_file == nullptr) {
+        perror("fopen");
+    }
+
+    stat(path.c_str(), &sb);
+    res.resize(sb.st_size);
+    fread(const_cast<char*>(res.data()), sb.st_size, 1, input_file);
+    fclose(input_file);
+
+    return res;
+}
+} // namespace imp
