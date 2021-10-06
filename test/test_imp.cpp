25f43b321de8a054f0366fdf08bf29ee3a2ab6d8
Alecto Irene Perez <perez.cs@pm.me>
2021-10-06 14:10:06 -0600

    Added test for read_file
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- test/test_imp.cpp
+++ test/test_imp.cpp
@@ -1,6 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
+#include <imp/read_file.hpp>
 
-
+TEST_CASE("Test read_file") {
+    REQUIRE(imp::read_file("test/test_files/hello.txt") == "Hello, world!\n");
+}
 TEST_CASE("Tests set up correctly") {
     REQUIRE(true);
 }
