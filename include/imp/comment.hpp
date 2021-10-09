dd914b7294a78c136b9c441b15d01ed162b0588c
jorgeypcb <jorge.loredx@gmail.com>
2021-10-09 14:53:05 -0600

    correcting the parser for the comments

--- include/imp/comment.hpp
+++ include/imp/comment.hpp
@@ -1,6 +1,6 @@
+#include <noam/combinators.hpp>
 #include <noam/parser.hpp>
 #include <noam/result_types.hpp>
-#include <noam/combinators.hpp>
 
 namespace imp {
 using noam::parser;
@@ -10,24 +10,30 @@ constexpr parser multiline_comment {
     [](std::string_view st) -> result<std::string_view> {
         if (st.starts_with("{-")) {
             size_t found = st.find("-}", 2);
+            if (found == st.npos) {
+                return {};
+            }
             return {st.substr(found + 2), st.substr(2, found)};
         } else {
             return {};
         }
-    }
-};
+    }};
 
 constexpr parser singleline_comment {
     [](std::string_view st) -> result<std::string_view> {
         if (st.starts_with("--")) {
             size_t found = st.find("\n", 2);
-            return {st.substr(found + 2), st.substr(2, found)};
+            if (found == st.npos) {
+                return {};
+            }
+            return {st.substr(found + 1), st.substr(2, found)};
         } else {
             return {};
         }
-    }
-};
+    }};
 
-constexpr auto comment =
-    noam::either<std::string_view> (multiline_comment, singleline_comment);
+constexpr auto comment = noam::either<std::string_view>(
+    multiline_comment,
+    singleline_comment);
+
 } // namespace imp
