ae55002b3408a77b9ff8cbbc0c5ea27cbcf2356d
slinky57 <darredondo@unm.edu>
2021-10-22 11:37:51 -0600

    mov ast test to src, iterating ast_to_dotfile

--- include/imp/ast_printing.hpp
+++ include/imp/ast_printing.hpp
@@ -10,27 +10,28 @@ namespace imp{
 using std::string;
 using std::ostream;
 
-void print_command(command const& cmd, std::ofstream out);
 
-void ast_to_file(string fname, command const& ast) {
+ostream& operator<<(ostream& os, command const& expr) {
+    auto visitor = [&](auto const& v) { 
+        os << v; 
+    };
+    rva::visit(visitor, expr);
+    return os;
+}
+
+void ast_to_dotfile(string fname, command const& ast) {
     std::ofstream dotfile;
     int new_node = 0;
-    dotfile.open (fname + ".dot");
+    dotfile.open (fname);
 
-    //// PASS OPEN DOTFILE TO OVERLOADED PRINT_COMMAND WITH NESTED CALLS TO PRINT THE REST OF THE AST
     dotfile << ast;
 
     dotfile.close();
 }
 
-ostream& operator<<(ostream& os, command const& ast) {return os; };
-
 ostream& operator<<(ostream& os, assignment<arith_expr> const& a) {
-    os << a.dest << "->" << a.value;
-}
-
-void print_command(assignment<arith_expr> const& a, std::ofstream out) {
-    out << a.dest << "->" << a.value; ///NEED TO FORMAT THIS TO GRAPHVIZ
+    os << "asdf";
+    return os;
 }
 
 } //namespace imp
