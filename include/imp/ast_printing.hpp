0264ed413cbb80ad2f45e6bc6c56b6c1a93722cb
slinky57 <darredondo@unm.edu>
2021-10-23 14:20:43 -0600

    declare_nodes for all types

--- include/imp/ast_printing.hpp
+++ include/imp/ast_printing.hpp
@@ -23,8 +23,17 @@ size_t get_id(const rva::variant<T...>& anything) {
 }
 
 // declare_nodes forward declarations
-void declare_nodes(ostream& os, arith_expr const& node);
-void declare_nodes(ostream& os, command const& node);
+
+
+template <class... T>
+void declare_nodes(ostream& os, rva::variant<T...> const& node);
+// void declare_nodes(ostream& os, arith_expr const& node);
+
+// void declare_nodes(ostream& os, binary_expr<arith_expr> const& node);
+
+// void declare_nodes(ostream& os, bool_expr const& node);
+
+// void declare_nodes(ostream& os, command const& node);
 
 // declare_node implementations
 void declare_nodes(ostream& os, constant const& node) {
@@ -39,24 +48,69 @@ void declare_nodes(ostream& os, assignment<arith_expr> const& node) {
     declare_nodes(os, node.value);
 }
 
+void declare_nodes(ostream& os, binary_expr<arith_expr> const& node) {
+    os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
+    declare_nodes(os, node.get_left());
+    declare_nodes(os, node.get_right());
+}
+
+// os << get_id(node) << " [label = \"" <<  << "\"];\n";
+void declare_nodes(ostream& os, bool_const const& node){
+    os << get_id(node) << " [label = \"" << node.value << "\"];\n";
+}
+
+void declare_nodes(ostream& os, unary_expr<bool_expr> const& node){
+    os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
+    declare_nodes(os, node.get_input());
+}
+
+void declare_nodes(ostream& os, binary_expr<bool_expr> const& node){
+    os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
+    declare_nodes(os, node.get_left());
+    declare_nodes(os, node.get_right());
+}
+
+void declare_nodes(ostream& os, const imp::skip_command& node) {
+    os << get_id(node) << " [label = \"" << "skip" << "\"];\n";
+}
+
+void declare_nodes(ostream& os, if_command<bool_expr, command> const& node){
+    os << get_id(node) << " [label = \"" << "if_condition" << "\"];\n";
+    declare_nodes(os, node.get_condition());
+    declare_nodes(os, node.when_false());
+    declare_nodes(os, node.when_true());
+} 
+
+void declare_nodes(ostream& os, while_loop<bool_expr, command> const& node){
+    os << get_id(node) << " [label = \"" << "while_condition" << "\"];\n";
+    declare_nodes(os, node.get_condition());
+    declare_nodes(os, node.get_body());
+}
+
 void declare_nodes(ostream& os, std::vector<command> const& victor) {
     os << get_id(victor) << " [label = \"list of commands\"];\n";
     for (auto& item : victor) {
         declare_nodes(os, item);
     }
 }
-void declare_nodes(ostream& os, arith_expr const& node) {
+
+template <class... T>
+void declare_nodes(ostream& os, rva::variant<T...> const& node) {
     std::cout << "declare nodes\n";
     auto visitor = [&](auto const& v) { declare_nodes(os, v); };
     rva::visit(visitor, node);
 }
+// void declare_nodes(ostream& os, arith_expr const& node) {
+//     std::cout << "declare nodes\n";
+//     auto visitor = [&](auto const& v) { declare_nodes(os, v); };
+//     rva::visit(visitor, node);
+// }
 
-
-void declare_nodes(ostream& os, command const& node) {
-    std::cout << "declare nodes\n";
-    auto visitor = [&](auto const& v) { declare_nodes(os, v); };
-    rva::visit(visitor, node);
-}
+// void declare_nodes(ostream& os, command const& node) {
+//     std::cout << "declare nodes\n";
+//     auto visitor = [&](auto const& v) { declare_nodes(os, v); };
+//     rva::visit(visitor, node);
+// }
 
 // operator<< forward declarations
 
@@ -67,6 +121,12 @@ ostream& operator<<(ostream& os, command const& expr);
 ostream& operator<<(ostream& os, assignment<arith_expr> const& a) {
     os << get_id(a) << "->" << get_id(a.dest) << '\n';
     os << get_id(a) << "->" << get_id(a.value) << '\n';
+    return os;
+}
+
+ostream& operator<<(ostream& os, binary_expr<arith_expr> const& a) {
+    os << get_id(a) << "->" << get_id(a.get_left()) << '\n';
+    os << get_id(a) << "->" << get_id(a.get_right()) << '\n';
     return os;
 }
 
