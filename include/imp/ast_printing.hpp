2ada97838e76121a9eb386ee8ba30705750ffdf5
slinky57 <darredondo@unm.edu>
2021-10-23 14:59:23 -0600

    ast printing all syntax types (while bug present)

--- include/imp/ast_printing.hpp
+++ include/imp/ast_printing.hpp
@@ -27,6 +27,8 @@ size_t get_id(const rva::variant<T...>& anything) {
 
 template <class... T>
 void declare_nodes(ostream& os, rva::variant<T...> const& node);
+template <class... T>
+ostream& operator<<(ostream& os, rva::variant<T...> const& expr);
 // void declare_nodes(ostream& os, arith_expr const& node);
 
 // void declare_nodes(ostream& os, binary_expr<arith_expr> const& node);
@@ -49,42 +51,88 @@ void declare_nodes(ostream& os, assignment<arith_expr> const& node) {
 }
 
 void declare_nodes(ostream& os, binary_expr<arith_expr> const& node) {
-    os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
+    os << get_id(node) << " [label = \"" << "binary_expr<arith_expr> " << node.get_op() << "\"];\n";
     declare_nodes(os, node.get_left());
     declare_nodes(os, node.get_right());
 }
 
 // os << get_id(node) << " [label = \"" <<  << "\"];\n";
-void declare_nodes(ostream& os, bool_const const& node){
+void declare_nodes(ostream& os, bool_const const& node) {
     os << get_id(node) << " [label = \"" << node.value << "\"];\n";
 }
 
-void declare_nodes(ostream& os, unary_expr<bool_expr> const& node){
+ostream& operator<<(ostream& os, bool_const const& a) { return os; }
+
+void declare_nodes(ostream& os, unary_expr<bool_expr> const& node) {
     os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
     declare_nodes(os, node.get_input());
 }
 
-void declare_nodes(ostream& os, binary_expr<bool_expr> const& node){
+ostream& operator<<(ostream& os, unary_expr<bool_expr> const& a) {
+    os << get_id(a) << "->" << get_id(a.get_input()) << "\n";
+    os << a.get_input();
+    return os;
+}
+
+void declare_nodes(ostream& os, binary_expr<bool_expr> const& node) {
     os << get_id(node) << " [label = \"" << node.get_op() << "\"];\n";
     declare_nodes(os, node.get_left());
     declare_nodes(os, node.get_right());
 }
 
-void declare_nodes(ostream& os, const imp::skip_command& node) {
-    os << get_id(node) << " [label = \"" << "skip" << "\"];\n";
+ostream& operator<<(ostream& os, binary_expr<bool_expr> const& a) {
+    os << get_id(a) << "->" << get_id(a.get_left()) << "\n";
+    os << get_id(a) << "->" << get_id(a.get_right()) << "\n";
+    os << a.get_left();
+    os << a.get_right();
+    return os;
 }
 
-void declare_nodes(ostream& os, if_command<bool_expr, command> const& node){
-    os << get_id(node) << " [label = \"" << "if_condition" << "\"];\n";
+void declare_nodes(ostream& os, const imp::skip_command& node) {
+    os << get_id(node) << " [label = \""
+       << "skip"
+       << "\"];\n";
+}
+
+ostream& operator<<(ostream& os, skip_command const& a) { return os; }
+
+ostream& operator<<(ostream& os, variable const& a) { return os; }
+
+ostream& operator<<(ostream& os, constant const& a) { return os; }
+
+void declare_nodes(ostream& os, if_command<bool_expr, command> const& node) {
+    os << get_id(node) << " [label = \""
+       << "if_condition"
+       << "\"];\n";
     declare_nodes(os, node.get_condition());
     declare_nodes(os, node.when_false());
     declare_nodes(os, node.when_true());
-} 
+}
 
-void declare_nodes(ostream& os, while_loop<bool_expr, command> const& node){
-    os << get_id(node) << " [label = \"" << "while_condition" << "\"];\n";
+ostream& operator<<(ostream& os, if_command<bool_expr, command> const& a) {
+    os << get_id(a) << "->" << get_id(a.get_condition()) << "\n";
+    os << get_id(a) << "->" << get_id(a.when_false()) << "\n";
+    os << get_id(a) << "->" << get_id(a.when_true()) << "\n";
+    os << a.get_condition();
+    os << a.when_false();
+    os << a.when_true();
+    return os;
+}
+
+void declare_nodes(ostream& os, while_loop<bool_expr, command> const& node) {
+    os << get_id(node) << " [label = \""
+       << "while_condition"
+       << "\"];\n";
     declare_nodes(os, node.get_condition());
     declare_nodes(os, node.get_body());
+}
+
+ostream& operator<<(ostream& os, while_loop<bool_expr, command> const& a) {
+    os << get_id(a) << "->" << get_id(a.get_condition()) << "\n";
+    os << get_id(a) << "->" << get_id(a.get_body()) << "\n";
+    os << a.get_condition();
+    os << a.get_body();
+    return os;
 }
 
 void declare_nodes(ostream& os, std::vector<command> const& victor) {
@@ -114,19 +162,22 @@ void declare_nodes(ostream& os, rva::variant<T...> const& node) {
 
 // operator<< forward declarations
 
-ostream& operator<<(ostream& os, command const& expr);
-
 // operator<< implementations
 
-ostream& operator<<(ostream& os, assignment<arith_expr> const& a) {
-    os << get_id(a) << "->" << get_id(a.dest) << '\n';
-    os << get_id(a) << "->" << get_id(a.value) << '\n';
-    return os;
-}
+
 
 ostream& operator<<(ostream& os, binary_expr<arith_expr> const& a) {
     os << get_id(a) << "->" << get_id(a.get_left()) << '\n';
     os << get_id(a) << "->" << get_id(a.get_right()) << '\n';
+    os << a.get_left();
+    os << a.get_right();
+    return os;
+}
+
+ostream& operator<<(ostream& os, assignment<arith_expr> const& a) {
+    os << get_id(a) << "->" << get_id(a.dest) << '\n';
+    os << get_id(a) << "->" << get_id(a.value) << '\n';
+    os << a.value;
     return os;
 }
 
@@ -138,7 +189,8 @@ ostream& operator<<(ostream& os, std::vector<command> const& victor) {
     return os;
 }
 
-ostream& operator<<(ostream& os, command const& expr) {
+template <class... T>
+ostream& operator<<(ostream& os, rva::variant<T...> const& expr) {
     std::cout << "calling visitor\n";
     auto visitor = [&](auto const& v) { os << v; };
     rva::visit(visitor, expr);
--- src/ast_printing_test.cpp
+++ src/ast_printing_test.cpp
@@ -10,12 +10,62 @@ int main() {
 
     // command test_command = assignment<arith_expr> { variable{"x"},
     // constant{10} };
-    command test_command = command {std::vector<command> {
-        assignment<arith_expr> {variable {"x"}, constant {10}},
-        assignment<arith_expr> {variable {"z"}, constant {20}},
-        command {std::vector<command> {
-            assignment<arith_expr> {variable {"x"}, constant {10}},
-            assignment<arith_expr> {variable {"z"}, constant {20}}}}}};
+
+    // command test_command = command {std::vector<command> {
+    //     assignment<arith_expr> {variable {"x"}, constant {10}},
+    //     assignment<arith_expr> {variable {"z"}, constant {20}},
+    //     command {std::vector<command> {
+    //         assignment<arith_expr> {variable {"x"}, constant {10}},
+    //         assignment<arith_expr> {variable {"z"}, constant {20}}}}}};
+
+    command test_command = command {
+        std::vector<command> {
+            assignment<arith_expr> {
+                variable {"x"}, // <br>
+                constant {10}   // <br>
+            },                  // <br>
+            assignment<arith_expr> {
+                variable {"y"}, // <br>
+                constant {20}   // <br>
+            },                  // <br>
+            assignment<arith_expr> {
+                variable {"z"}, // <br>
+                arith_expr {
+                    binary_expr<arith_expr> {
+                        binary_expr<arith_expr>::data {
+                            variable {"x"}, // <br>
+                            variable {"y"}, // <br>
+                            '+'             // <br>
+                        }                   // <br>
+                    }                       // <br>
+                }                           // <br>
+            },                              // <br>
+            while_loop<bool_expr, command> {
+                while_loop<bool_expr, command>::data {
+                    binary_expr<arith_expr> {
+                        binary_expr<arith_expr>::data {
+                            variable {"x"}, // <br>
+                            variable {"y"}, // <br>
+                            '>'             // <br>
+                        }                   // <br>
+                    },                      // <br>
+                    assignment<arith_expr> {
+                        variable {"x"}, // <br>
+                        arith_expr {
+                            binary_expr<arith_expr> {
+                                binary_expr<arith_expr>::data {
+                                    variable {"x"}, // <br>
+                                    constant {1},   // <br>
+                                    '+'             // <br>
+                                }                   // <br>
+                            }                       // <br>
+                        }                           // <br>
+                    }                               // <br>
+                }                                   // <br>
+            }                                       // <br>
+        }                                           // <br>
+    };
+
 
     ast_to_dotfile("testdot.dot", test_command);
 }
