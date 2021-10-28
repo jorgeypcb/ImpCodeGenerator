7a90325f9cd21aaedd06f936334c914e72ecf1fc
Alecto Irene Perez <perez.cs@pm.me>
2021-10-27 21:18:30 -0600

    Create print_op_file.cpp
    This produces target print_op_file. This prints a sample file of
    operations to stdout.
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

---
+++ src/print_op_file.cpp
@@ -0,0 +1,9 @@
+#include <imp/instruction.hpp>
+
+int main() {
+    using imp::Op;
+    for(int i = 0; i <= int(Op::Label); i++) {
+        auto ins = imp::instruction{Op{i}, 1, 2, 3};
+        fmt::print("{}\n", ins);
+    }
+}
