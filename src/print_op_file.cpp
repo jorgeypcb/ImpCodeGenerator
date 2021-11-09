b50ec0943141d15c0cda828321cf7525bdd540ad
Alecto Irene Perez <perez.cs@pm.me>
2021-11-09 14:42:08 -0700

    Add jump instruction print_op_file
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/print_op_file.cpp
+++ src/print_op_file.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     using imp::Op;
-    for(int i = 0; i <= int(Op::Label); i++) {
+    for(int i = 0; i <= int(Op::Jump); i++) {
         auto ins = imp::instruction{Op{i}, 1, 2, 3};
         fmt::print("{}\n", ins);
     }
