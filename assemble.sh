f3ef3af59b834ba2ad34468b8542077e81f758f7
Alecto Irene Perez <perez.cs@pm.me>
2021-12-12 00:49:31 -0700

    Update assemble.sh to produce assembly

--- assemble.sh
+++ assemble.sh
@@ -4,4 +4,8 @@ for file in programs/*.imp; do
     build/assembler $file     \
         1> ${file%.imp}.ins   \
         2> ${file%.imp}.vars
+    riscv/print_risc.py ${file} \
+        --constant_folding      \
+        --dead_code_elim        \
+        --register_allocation > ${file%.imp}.asm
 done
